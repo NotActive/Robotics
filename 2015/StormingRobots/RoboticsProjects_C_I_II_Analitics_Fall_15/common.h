512ed55c63b77735151008ce181c1f1d6826fa54
Proromayev <peter.romayev@gmail.com>
2015-10-15 14:54:28 -0400

    Added float

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
@@ -3,5 +3,4 @@ const float Wdia = 5.6;
 const float EncPerCm = 360.0*GR/(Wdia*PI);
 const	float InnerCircleDiameter = 20.0;
 const	float OuterMotorSpeed = 100.0;
-
-int RobotBase = 5;
+const float RobotBase = 5.0;
