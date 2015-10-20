3722c7ae608263530adba8a8f2f82c7e05bd6bed
Proromayev <peter.romayev@gmail.com>
2015-10-20 15:37:37 -0400

    Fixed up bugs in common.h, Function.c, main.c

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Function.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Function.c
@@ -2,21 +2,68 @@
 
 void MotorEncoderReset()
 {
-	resetMotorEncoder(motorB);
-	resetMotorEncoder(motorC);
+	nMotorEncoder[Lm] = 0;
+	nMotorEncoder[Rm] = 0;
 }
 
-void TurnHalfCircle ()
-{
-	float OuterCircleDiameter = InnerCircleDiameter+(RobotBase*2.0);
-	float InnerMotorSpeed = (InnerCircleDiameter/OuterCircleDiameter)*OuterMotorSpeed;
-	float InnerMotorEnc = (InnerCircleDiameter/2.0)*EncPerCm;
-	float OuterMotorEnc = (OuterCircleDiameter/2.0)*EncPerCm;
+//void TurnHalfCircle ()
+//{
+//	float OuterCircleDiameter = InnerCircleDiameter+(RobotBase*2.0);
+//	float InnerMotorSpeed = (InnerCircleDiameter/OuterCircleDiameter)*Speed;
+//	float InnerMotorEnc = (InnerCircleDiameter/2.0)*EncPerCm;
+//	float OuterMotorEnc = (OuterCircleDiameter/2.0)*EncPerCm;
+//	MotorEncoderReset();
+//	nMotorEncoder[motorB] = (int)OuterMotorEnc;
+//	nMotorEncoder[motorC] = (int)InnerMotorEnc;
+//	motor[motorB] = (int)Speed;
+//	motor[motorC] = (int)InnerMotorSpeed;
+//}
 
+void GoForward()
+{
 	MotorEncoderReset();
 
-	nMotorEncoder[motorB] = (int)OuterMotorEnc;
-	nMotorEncoder[motorC] = (int)InnerMotorEnc;
-	motor[motorB] = (int)OuterMotorSpeed;
-	motor[motorC] = (int)InnerMotorSpeed;
+	if(nMotorEncoder[motorB]>Dist){
+		motor[motorB] = Speed;
+		motor[motorC] = Speed;
+	}
 }
+
+		//void GoForward(float Dist)
+		//{
+		//	MotorEncoderReset();
+		//	int total = Dist * EncPerCm;
+		//	setMotorTarget(Lm, total, Speed);
+		//	setMotorTarget(Rm, total, Speed);
+		//	waitUntilMotorStop(Rm);
+		//	wait1Msec(100);
+		//}
+
+		//void GoBackward(float Dist)
+		//{
+		//	MotorEncoderReset();
+		//	int total = -Dist * EncPerCm;
+		//	setMotorTarget(Lm, total, Speed);
+		//	setMotorTarget(Rm, total, Speed);
+		//	waitUntilMotorStop(Lm);
+		//}
+
+		//void TurnLeft(float Deg)
+		//{
+		//	MotorEncoderReset();
+		//	int total = Deg * EncPerDeg;
+		//	setMotorTarget(Lm, total, -Speed);
+		//	setMotorTarget(Rm, total, Speed);
+		//	waitUntilMotorStop(Rm);
+		//	wait1Msec(100);
+		//}
+
+		//void TurnRight(float Deg)
+		//{
+		//	MotorEncoderReset();
+		//	int total = Deg * EncPerDeg;
+		//	setMotorTarget(Lm, total, Speed);
+		//	setMotorTarget(Rm, total, -Speed);
+		//	waitUntilMotorStop(Rm);
+		//	wait1Msec(100);
+		//}
--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/main.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/main.c
@@ -2,5 +2,5 @@
 
 task main()
 {
-    TurnHalfCircle();
+    GoForward();
 }
