b8cb8599d72260d896a82cf7e4bc649a1bb8801b
Proromayev <peter.romayev@gmail.com>
2015-10-29 17:31:37 -0400

    Made ZombieDetector log files

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/ZombieDetector.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/ZombieDetector.c
@@ -1,3 +1,5 @@
+#define MAX_FILE_SIZE  5000  //maximum file size in bytes
+
 task main()
 {
 	const int Lm = motorB;
@@ -10,15 +12,57 @@ task main()
 	const float Speed = 50.0;
 	const float EncPerCm = 360.0*GR/(WheelDia*PI);
 
+	nMotorEncoder[Lm] = 0;
 	SensorType[SonarSensor] = sensorSONAR;
 
-	nMotorEncoder[Lm] = 0;
+	//logging
+	int bytes = 0;
+	char fileName[15];
+
+#if defined(NXT)
+	TFileHandle fp;              // to store file handler
+	TFileIOResult result;        // to store status
+#else
+	long fp;
+#endif
+
+	// Reset NXT time
+	clearTimer(T1);
+	time1(T1) = 0;
+
+	// create a file name based on the current System time
+	sprintf(fileName,"log%d.txt", nSysTime/1000);
+
+	// create or open file
+#if defined(NXT)
+	Delete(fileName, result);
+	short fileSize = MAX_FILE_SIZE;
+	OpenWrite(fp, result, fileName, fileSize);
+#else
+	fp = fileOpenWrite(fileName);
+#endif
 
 	while(nMotorEncoder[Lm] <= Dist*EncPerCm)
 	{
 		motor[Lm] = Speed;
 		motor[Rm] = Speed;
-		displayTextLine(5, "%d", SensorValue[SonarSensor]);
-		displayTextLine(4, "%d", nMotorEncoder[Lm]);
+
+		// create message
+		string message;
+		sprintf(message, "%d\t%d\r\n", nMotorEncoder[Lm], SensorValue[SonarSensor]);
+
+		// write message
+#if defined(NXT)
+		WriteText(fp, result, message);
+#else
+		fileWriteData(fp, message, messagelen(message));
+#endif
 	}
+
+	// close file
+#if defined(NXT)
+	Close(fp, result);
+#else
+	fileClose(fp);
+#endif
 }
