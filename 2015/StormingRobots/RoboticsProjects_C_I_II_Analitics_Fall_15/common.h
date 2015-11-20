26ed24b26cedfb7c68becc1f39d251303202f58d
Proromayev <peter.romayev@gmail.com>
2015-11-20 16:18:37 -0500

    Update common.h

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
@@ -1,10 +1,10 @@
 const float GR = 3.0/5.0;
-const float Wdia = 3.3;
-const float RobotBase = 15.5;
+const float Wdia = 3.5;
+const float RobotBase = 15.6;
 const float EncPerCm = (360.0*GR)/(Wdia*PI);
 const float EncPerDeg = RobotBase/Wdia;
 
-const int Lm = motorB;
-const int Rm = motorC;
+const tMotor Lm = motorB;
+const tMotor Rm = motorC;
 
 float InnerCircleDiameter = 20.0;
