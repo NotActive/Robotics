dcd4d7a4830fa403ba5e68417b09baa6b523cdcc
Proromayev <peter.romayev@gmail.com>
2015-12-04 15:49:48 -0500

    Update common.h

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/common.h
@@ -2,7 +2,7 @@ const float GR = 3.0/5.0;
 const float Wdia = 3.5;
 const float RobotBase = 15.6;
 const float EncPerCm = (360.0*GR)/(Wdia*PI);
-const float EncPerDeg = RobotBase/Wdia;
+const float EncPerDeg = RobotBase*GR/Wdia;
 
 const tMotor Lm = motorB;
 const tMotor Rm = motorC;
