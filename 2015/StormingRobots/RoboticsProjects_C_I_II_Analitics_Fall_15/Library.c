10edc7db53ebee4a0c4a88ef5073d81e38425aa3
Proromayev <peter.romayev@gmail.com>
2015-12-04 15:49:06 -0500

    Update Library.c

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Library.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Library.c
@@ -3,11 +3,12 @@
 float speed = 100.0;
 
 #define resetMotorEncoder(m)  motor[m] = 0
+#define stillRunning(m)   (nMotorRunState[m]!=runStateIdle)
 
 void MotorEncoderReset()
 {
-	resetMotorEncoder(motorB);
-	resetMotorEncoder(motorC);
+	resetMotorEncoder(Lm);
+	resetMotorEncoder(Rm);
 }
 
 #define setMotorTarget(m, enc, s)   { nMotorEncoderTarget[m] = enc; motor[m] = s;}
@@ -18,7 +19,7 @@ void GoForward(float dist)
 	int total = dist * EncPerCm;
 	setMotorTarget(Lm, total, speed);
 	setMotorTarget(Rm, total, speed);
-	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
+	while ( stillRunning(Lm) && stillRunning(Rm) )
 		sleep(100);
 	wait1Msec(100);
 }
@@ -29,7 +30,8 @@ void GoBackward(float dist)
 	int total = dist * EncPerCm;
 	setMotorTarget(Lm, total, -speed);
 	setMotorTarget(Rm, total, -speed);
-	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
+
+	while ( stillRunning(Lm)  && stillRunning(Rm) )
 		sleep(100);
 }
 
@@ -39,7 +41,7 @@ void TurnLeft(float deg)
 	int total = deg * EncPerDeg;
 	setMotorTarget(Lm, total, -speed);
 	setMotorTarget(Rm, total, speed);
-	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
+	while ( stillRunning(Lm) && stillRunning(Rm) )
 		sleep(100);
 	wait1Msec(100);
 }
@@ -50,7 +52,7 @@ void TurnRight(float deg)
 	int total = deg * EncPerDeg;
 	setMotorTarget(Lm, total, speed);
 	setMotorTarget(Rm, total, -speed);
-	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
+	while ( stillRunning(Lm) && stillRunning(Rm) )
 		sleep(100);
 	wait1Msec(100);
 }
@@ -60,25 +62,37 @@ void MakeSTurn( float innerDiameter)
 	int speed = 100;
 	MotorEncoderReset();
 
+	clearDebugStream();
+	writeDebugStreamLine("Opower Ipower | OEnc   IEnc ");
+	writeDebugStreamLine("============================");
+
 	const float outerCircleDiameter = (RobotBase*2.0)+innerDiameter;
-	const float innerMotorSpeed = (innerDiameter/outerCircleDiameter)*speed;
-	const float innerMotorEnc = ((innerDiameter*PI)/2)*EncPerCm;
-	const float outerMotorEnc = ((outerCircleDiameter*PI)/2)*EncPerCm;
+
+	float ratio = outerCircleDiameter/innerDiameter;
+	
+	const float innerMotorSpeed = ratio*speed;
+	//const float innerMotorEnc = 1234;//((innerDiameter*PI)/2)*EncPerCm;
+	//const float outerMotorEnc = 3159.04//((outerCircleDiameter*PI)/2)*EncPerCm;
+	const float innerMotorEnc = (innerDiameter*PI)/2 * EncPerCm  * ratio;
+	const float outerMotorEnc = (outerCircleDiameter*PI) / 2 * EncPerCm * ratio;
+
+	writeDebugStreamLine("%f %f | %d %d ", speed, innerMotorSpeed, outerMotorEnc,  innerMotorEnc );
 
 	setMotorTarget(Rm, outerMotorEnc, speed);
 	setMotorTarget(Lm, innerMotorEnc, innerMotorSpeed);
+	
+
 
 	//while    lm stop  running   &&  rm stop running
-	
- // while ( nMotorRunState[Lm] != runStateIdle  ||  	nMotorRunState[Lm] != runStateIdle )  
-	
-	//while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
-	
-  while ( !( nMotorRunState[Lm] == runStateIdle  &&  	nMotorRunState[Rm] == runStateIdle) ) 	
-   	sleep(100);
-   displayTextLine(6, "T-I:%.2f", innerMotorEnc);
-  displayTextLine(7, "T-O:%.2f", outerMotorEnc);
- 
+
+	// while ( nMotorRunState[Lm] != runStateIdle  ||  	nMotorRunState[Lm] != runStateIdle )
+
+	//while ( stillRunning(Lm) && stillRunning(Rm) )
+
+	while ( !( nMotorRunState[Lm] == runStateIdle  &&  	nMotorRunState[Rm] == runStateIdle) )
+  while (stillRunning(Rm) || stillRunning(Lm))
+		sleep(100);
+
 	wait1Msec(100);
 
 }
