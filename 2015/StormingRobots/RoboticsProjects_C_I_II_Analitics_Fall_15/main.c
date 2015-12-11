5a9f1f9c69a950d7a286131b73f07d37524d5c43
Proromayev <peter.romayev@gmail.com>
2015-12-11 15:59:52 -0500

    Update main.c

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/main.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/main.c
@@ -1,32 +1,57 @@
 #include "Library.c"
 
+void runEnc( int enc)
+{
+	int speed = 90;
+	MotorEncoderReset();
+
+	const float outerCircleDiameter = (RobotBase*2.0)+innerDiameter;
+
+	const float outerMotorEnc = ((outerCircleDiameter*PI)/2)*EncPerCm;
+	const float innerMotorSpeed = (innerDiameter/outerCircleDiameter)*speed;
+	const float innerMotorEnc = ((innerDiameter*PI)/2)*EncPerCm;
+
+	// Which motor is the master: true -> outer, false -> inner
+	bool outerMotorMaster = true;
+
+	tMotor master;
+	int targetEncoders;
+  string message;
+		
+		master = Rm;
+		targetEncoders = enc;
+		nSyncedMotors = syncRmLm;
+		nSyncedTurnRatio = 100.0 * innerDiameter / outerCircleDiameter;
+		
+	delay(2000);
+		
+	nMotorEncoderTarget[master] = targetEncoders;
+	motor[master] = speed;
+
+	//while(nMotorEncoder[master] < targetEncoders)
+	//{
+	//	wait1Msec(10);
+	//}
+	while ( stillRunning(Rm) )
+		sleep(100);
+	wait1Msec(10);
+	nSyncedMotors = synchNone;          // turn off the motor feedback control
+
+	wait1Msec(100);
+}
+
 task main()
 {
+	string message;
+	//while (true)
+	{
 
-<<<<<<< HEAD
-MakeSTurn(20.0);
-int i=0;
-string message;
-while (true)
-{
-	sprintf(message, "inner %d outer %d", nMotorEncoder[Lm], nMotorEncoder[Rm]);
-	writeDebugStreamLine("%s", message);
-	delay(2000);
-=======
-	MakeSTurn(20.0);
+		MakeSTurn(20.0);
 
-	displayTextLine(2, "Lenc:%d", nMotorEncoder[Lm]);
-	displayTextLine(3, "Renc: %d", nMotorEncoder[Rm]);
-	delay(50000);
->>>>>>> origin/master
+		displayTextLine(2, "Lenc:%d", nMotorEncoder[Lm]);
+		displayTextLine(3, "Renc: %d", nMotorEncoder[Rm]);
+		runEnc(528);
+		delay(50000);
+
+	}
 }
-	//float dist = 50.0;
-	//float deg = 90.0;
-
-	//int motorencoder = nMotorEncoder[Lm];
-
-	////displayTextLine(2, "Lenc:%d", nMotorEncoder[Lm]);
-	////displayTextLine(3, "Renc: %d", nMotorEncoder[Rm]);
-	////displayTextLine(4, "Bat: %d", nAvgBatteryLevel);
-	//delay(50000);
- }
