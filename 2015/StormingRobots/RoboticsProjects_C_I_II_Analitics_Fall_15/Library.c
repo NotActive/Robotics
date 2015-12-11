461dfe294aa8cfd326e84d42ec37acc15e001768
Proromayev <peter.romayev@gmail.com>
2015-12-11 16:00:18 -0500

    Update Library.c

--- 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Library.c
+++ 2015/StormingRobots/RoboticsProjects_C_I_II_Analitics_Fall_15/Library.c
@@ -13,49 +13,17 @@ void MotorEncoderReset()
 
 #define setMotorTarget(m, enc, s)   { nMotorEncoderTarget[m] = enc; motor[m] = s;}
 
-//void GoForward(float dist)
-//{
-//	MotorEncoderReset();
-//	int total = dist * EncPerCm;
-//	setMotorTarget(Lm, total, speed);
-//	setMotorTarget(Rm, total, speed);
-//	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
-//		sleep(100);
-//	wait1Msec(100);
-//}
-
-void goForward(float distInCm)
+void GoForward(float dist)
 {
-<<<<<<< HEAD
-  float target=distInCm/(PI*Wdia/360);
-  nMotorEncoder[Lm]=0;
-  nMotorEncoder[Rm]=0;
-
-  nSyncedMotors=synchBC; // turn on motor feedback control to keep two motors in sync
-                         // have motorA as master motor, motorB as slave motor
-  nSyncedTurnRatio=100;  // keep output of motorB 100% of output of motorA
-  nMotorEncoderTarget[Lm]=target;  // set the encoder target # ... does NOT start motor!
-  motor[Lm]=30;                    // must NOT modify the slave motor. Motor feedback control algorithm
-                                   // takes over modifying the slave motor.
-  while(nMotorEncoder[Lm]<target)
-  {
-    wait1Msec(10);
-  }
-  wait1Msec(500);
-  motor[Lm]=0;                      // as you see, you only stop the master motor.
-  wait1Msec(10);
-  nSyncedMotors=synchNone;          // turn off the motor feedback control
-                                    // must un-sync
-=======
 	MotorEncoderReset();
 	int total = dist * EncPerCm;
 	setMotorTarget(Lm, total, speed);
 	setMotorTarget(Rm, total, speed);
-	while ( stillRunning(Lm) && stillRunning(Rm) )
+	while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
 		sleep(100);
 	wait1Msec(100);
->>>>>>> origin/master
 }
+
 
 void GoBackward(float dist)
 {
@@ -90,94 +58,92 @@ void TurnRight(float deg)
 	wait1Msec(100);
 }
 
+//void MakeSTurn( float innerDiameter)
+//{
+//	int speed = 100;
+//	MotorEncoderReset();
+
+//	clearDebugStream();
+//	writeDebugStreamLine("Opower Ipower | OEnc   IEnc ");
+//	writeDebugStreamLine("============================");
+
+//	const float outerCircleDiameter = (RobotBase*2.0)+innerDiameter;
+
+//	float ratio = outerCircleDiameter/innerDiameter;
+
+//	const float innerMotorSpeed = ratio*speed;
+//	//const float innerMotorEnc = 1234;//((innerDiameter*PI)/2)*EncPerCm;
+//	//const float outerMotorEnc = 3159.04//((outerCircleDiameter*PI)/2)*EncPerCm;
+//	const float innerMotorEnc = (innerDiameter*PI)/2 * EncPerCm ;
+//	const float outerMotorEnc = (outerCircleDiameter*PI) / 2 * EncPerCm * ratio;
+
+//	writeDebugStreamLine("%f %f | %d %d ", speed, innerMotorSpeed, outerMotorEnc,  innerMotorEnc );
+
+//	setMotorTarget(Rm, outerMotorEnc, speed);
+//	setMotorTarget(Lm, innerMotorEnc, innerMotorSpeed);
+
+//	//while    lm stop  running   &&  rm stop running
+
+//	// while ( nMotorRunState[Lm] != runStateIdle  ||  	nMotorRunState[Lm] != runStateIdle )
+
+//	//while ( stillRunning(Lm) && stillRunning(Rm) )nMotorRunState
+
+//	while ( !( nMotorRunState[Lm] == runStateIdle  &&  	[Rm] == runStateIdle) )
+//  while (stillRunning(Rm) || stillRunning(Lm))
+//		sleep(100);
+
+//	wait1Msec(100);
+//}
+
 void MakeSTurn( float innerDiameter)
 {
 	int speed = 100;
 	MotorEncoderReset();
 
-	clearDebugStream();
-	writeDebugStreamLine("Opower Ipower | OEnc   IEnc ");
-	writeDebugStreamLine("============================");
-
 	const float outerCircleDiameter = (RobotBase*2.0)+innerDiameter;
-<<<<<<< HEAD
+
 	const float outerMotorEnc = ((outerCircleDiameter*PI)/2)*EncPerCm;
 	const float innerMotorSpeed = (innerDiameter/outerCircleDiameter)*speed;
 	const float innerMotorEnc = ((innerDiameter*PI)/2)*EncPerCm;
 
-  // Which motor is the master: true -> outer, false -> inner
-  bool outerMotorMaster = true;
+	// Which motor is the master: true -> outer, false -> inner
+	bool outerMotorMaster = true;
 
 	tMotor master;
-	float targetEncoders;
+	int targetEncoders;
+  string message;
+		
+	if (outerMotorMaster ==  true)
+	{
+		master = Rm;
+		targetEncoders = outerMotorEnc;
+		nSyncedMotors = syncRmLm;
+		nSyncedTurnRatio = 100.0 * innerDiameter / outerCircleDiameter;
+		sprintf(message, "Target OEnc=%d", targetEncoders);
+		}
+	else
+	{
+		master = Lm;
+		targetEncoders = innerMotorEnc;
+		nSyncedMotors = syncLmRm;
+		nSyncedTurnRatio = 100.0 * outerCircleDiameter / innerDiameter; // > 100% !!!
+		sprintf(message, "Target IEnc = %d", targetEncoders);
+	}
+	writeDebugStreamLine("%s", message);
+		
+	delay(2000);
+		
+	nMotorEncoderTarget[master] = targetEncoders;
+	motor[master] = speed;
 
-  if (outerMotorMaster ==  true)
-  {
-  	master = Rm;
-  	targetEncoders = outerMotorEnc;
-    nSyncedMotors = synchCB;
-    nSyncedTurnRatio = 100.0 * innerDiameter / outerCircleDiameter;
-  }
-  else
-  {
-  	master = Lm;
-  	targetEncoders = innerMotorEnc;
-    nSyncedMotors = synchBC;
-    nSyncedTurnRatio = 100.0 * outerCircleDiameter / innerDiameter; // > 100% !!!
-  }
-
-  nMotorEncoderTarget[master] = targetEncoders;
-  motor[master] = speed;
-
-  while(nMotorEncoder[master] < targetEncoders)
-  {
-    wait1Msec(10);
-  }
-  wait1Msec(500);
-  motor[master] = 0;                      // as you see, you only stop the master motor.
-  wait1Msec(10);
-  nSyncedMotors = synchNone;          // turn off the motor feedback control
-
-	//setMotorTarget(Rm, outerMotorEnc, speed);
-	//setMotorTarget(Lm, innerMotorEnc, innerMotorSpeed);
-=======
-
-	float ratio = outerCircleDiameter/innerDiameter;
-	
-	const float innerMotorSpeed = ratio*speed;
-	//const float innerMotorEnc = 1234;//((innerDiameter*PI)/2)*EncPerCm;
-	//const float outerMotorEnc = 3159.04//((outerCircleDiameter*PI)/2)*EncPerCm;
-	const float innerMotorEnc = (innerDiameter*PI)/2 * EncPerCm  * ratio;
-	const float outerMotorEnc = (outerCircleDiameter*PI) / 2 * EncPerCm * ratio;
-
-	writeDebugStreamLine("%f %f | %d %d ", speed, innerMotorSpeed, outerMotorEnc,  innerMotorEnc );
-
-	setMotorTarget(Rm, outerMotorEnc, speed);
-	setMotorTarget(Lm, innerMotorEnc, innerMotorSpeed);
-	
-
->>>>>>> origin/master
-
-	//while    lm stop  running   &&  rm stop running
-
-	// while ( nMotorRunState[Lm] != runStateIdle  ||  	nMotorRunState[Lm] != runStateIdle )
-
-<<<<<<< HEAD
-	//while ( (nMotorRunState[Lm] != runStateIdle)  &&  (nMotorRunState[Rm] != runStateIdle) )
-
-	//while ( nMotorRunState[Lm] != runStateIdle  || nMotorRunState[Rm] != runStateIdle ) {
-	//	sleep(100);
+	//while(nMotorEncoder[master] < targetEncoders)
+	//{
+	//	wait1Msec(10);
 	//}
-	//displayTextLine(6, "T-I:%.2f", innerMotorEnc);
-	//displayTextLine(7, "T-O:%.2f", outerMotorEnc);
-=======
-	//while ( stillRunning(Lm) && stillRunning(Rm) )
-
-	while ( !( nMotorRunState[Lm] == runStateIdle  &&  	nMotorRunState[Rm] == runStateIdle) )
-  while (stillRunning(Rm) || stillRunning(Lm))
+	while ( stillRunning(Rm) )
 		sleep(100);
->>>>>>> origin/master
+	wait1Msec(10);
+	nSyncedMotors = synchNone;          // turn off the motor feedback control
 
 	wait1Msec(100);
-
 }
